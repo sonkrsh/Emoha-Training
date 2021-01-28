ad54c10c0d2c3ece76230f0a281cbbef1ba06687
sonkrsh <sonkrsh@gmail.com>
2021-01-28 17:37:00 +0530

    Update index.js

--- app/containers/Sourav/index.js
+++ app/containers/Sourav/index.js
@@ -54,7 +54,7 @@ export function Sourav({ handleSubmit }) {
         <Form.Item
           label="Username"
           name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
+          rules={[{ required: true, message: 'Please input your username or Name!' }]}
         >
           <Input placeholder="hello" />
         </Form.Item>
