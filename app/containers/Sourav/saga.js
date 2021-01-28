333788a7f45105e480c42bb19df69fbabf8c3734
Sourav <sonkrsh@gmail.com>
2021-01-28 10:01:37 +0530

    fetch Form Data Using Selectors

--- app/containers/Sourav/saga.js
+++ app/containers/Sourav/saga.js
@@ -1,9 +1,13 @@
 import { take, call, put, select, takeLatest } from 'redux-saga/effects';
 import { DEFAULT_ACTION } from './constants';
+import {selectSouravDomain} from './selectors';
 
 function* myApiData(payload) {
-  console.log(payload.formvalue.username);
-  console.log(payload.formvalue.password);
+  const formData = yield select(selectSouravDomain);
+  console.log(formData.data.username)
+  console.log(formData.data.password)
+  /* console.log(payload.formvalue.username);
+  console.log(payload.formvalue.password); */
 }
 // Individual exports for testing
 export default function* souravSaga() {
